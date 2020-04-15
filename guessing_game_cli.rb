01835b289c4fb38ce435552f01bf003dfae67105
Patrick A <akolo@hotmail.com>
2020-04-15 03:21:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,11 @@ def run_guessing_game
   
   input = gets.chomp
 
+<<<<<<< HEAD
   if input == random_num.to_s 
+=======
+  if input == random_num
+>>>>>>> 5e499e1db943a80b4fd6388d15d17a15af659bd7
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{random_num}."
